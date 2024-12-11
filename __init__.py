32cb659a76b6df021fb4b429143e1b7ea13737d9
aNOniME01 <58111721+aNOniME01@users.noreply.github.com>
2024-12-11 19:19:41 +0000

    init fix

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 # __init__.py
+import os
+import bpy
+from . import addon_framework
+
 bl_info = {
     "name": "Ninja Tools",
     "blender": (4, 2, 3),
@@ -7,3 +11,22 @@ bl_info = {
     "version": (0, 0, 1),
     "author": "Nam-Nam"
 }
+
+# Get the absolute path of the addon directory
+addon_path = os.path.dirname(os.path.abspath(__file__))
+
+def register():
+    """Register the addon modules"""
+    # Load all modules from the 'modules' directory
+    loaded_modules = addon_framework.ModularAddonManager.load_modules(addon_path)
+    
+    # Register all loaded modules
+    addon_framework.ModularAddonManager.register_modules(loaded_modules)
+
+def unregister():
+    """Unregister the addon modules"""
+    # Load all modules again to unregister them
+    loaded_modules = addon_framework.ModularAddonManager.load_modules(addon_path)
+    
+    # Unregister all loaded modules
+    addon_framework.ModularAddonManager.unregister_modules(loaded_modules)
